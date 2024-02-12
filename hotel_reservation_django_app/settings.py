c1935f28e132870c1dee040f77767ff1f3de4680
Sina Roydel <3inaroydl@gmail.com>
2024-02-12 16:06:44 +0330

    add static files

--- hotel_reservation_django_app/settings.py
+++ hotel_reservation_django_app/settings.py
@@ -113,7 +113,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+MEDIA_URL = 'media/'
+
+# Path where media is stored'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
