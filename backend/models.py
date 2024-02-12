fb1ce540f20cbe2b52a61a9902ad90c2a266346d
Sina Roydel <3inaroydl@gmail.com>
2024-02-12 17:22:23 +0330

    create post and tag model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Post(models.Model):
+    tags = (
+        ('Hotel', 'Hotel'),
+        ('Single', 'Single'),
+        ('Family', 'Family'),
+        ('sea View', 'sea View'),
+
+    )
+
+    author = models.ForeignKey(get_user_model(),related_name="posts"
+                               ,on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    image1 = models.ImageField(upload_to="/images")
+    image2 = models.ImageField(upload_to="/images")
+
+class Tag(models.Model):
+    name = models.CharField(max_length=60)
+
+class TagPost(models.Model):
+    tag = models.ForeignKey(Tag,on_delete=models.CASCADE,primary_key=True)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,primary_key=True)
