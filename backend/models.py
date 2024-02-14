0ddaf3dc4996de8b0112e07eb3176db411a7e7be
Sina Roydel <3inaroydl@gmail.com>
2024-02-14 15:31:48 +0330

    create RestaurantItem model

--- backend/models.py
+++ backend/models.py
@@ -7,8 +7,8 @@ class Post(models.Model):
     author = models.ForeignKey(get_user_model(),related_name="posts"
                                ,on_delete=models.CASCADE)
     title = models.CharField(max_length=60)
-    image1 = models.ImageField(upload_to="./images")
-    image2 = models.ImageField(upload_to="./images")
+    image1 = models.ImageField(upload_to="./images/post/")
+    image2 = models.ImageField(upload_to="./images/post/")
     upload_date = models.DateTimeField(auto_now_add=True)
 
 class Tag(models.Model):
@@ -17,3 +17,16 @@ class Tag(models.Model):
 class TagPost(models.Model):
     tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+
+class RestaurantItem(models.Model):
+    categories = (
+        ("Breakfast","Breakfast"),
+        ("Lunch","Lunch"),
+        ("Dinner","Dinner"),
+        ("Drink","Drink"),
+    )
+    title = models.CharField(max_length=30)
+    desc = models.TextField()
+    category = models.CharField(max_length=20,choices=categories)
+    image = models.ImageField(upload_to='./images/food/')
