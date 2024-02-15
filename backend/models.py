f803d4902c54bdcafc4dd43a2cf3807516309bbc
Sina Roydel <3inaroydl@gmail.com>
2024-02-15 08:54:19 +0330

    create TeamMember model

--- backend/models.py
+++ backend/models.py
@@ -1,33 +1,40 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class Post(models.Model):
-
-
-    author = models.ForeignKey(get_user_model(),related_name="posts"
-                               ,on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), related_name="posts"
+                               , on_delete=models.CASCADE)
     title = models.CharField(max_length=60)
     image1 = models.ImageField(upload_to="./images/post/")
     image2 = models.ImageField(upload_to="./images/post/")
     upload_date = models.DateTimeField(auto_now_add=True)
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=60)
 
+
 class TagPost(models.Model):
-    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 class RestaurantItem(models.Model):
     categories = (
-        ("Breakfast","Breakfast"),
-        ("Lunch","Lunch"),
-        ("Dinner","Dinner"),
-        ("Drink","Drink"),
+        ("Breakfast", "Breakfast"),
+        ("Lunch", "Lunch"),
+        ("Dinner", "Dinner"),
+        ("Drink", "Drink"),
     )
     title = models.CharField(max_length=30)
     desc = models.TextField()
-    category = models.CharField(max_length=20,choices=categories)
+    category = models.CharField(max_length=20, choices=categories)
     image = models.ImageField(upload_to='./images/food/')
     price = models.PositiveSmallIntegerField()
+
+
+class TeamMember(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='./image/team/')
+    position = models.CharField(max_length=30)
