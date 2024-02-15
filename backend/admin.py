9664eedc6464e6c7202f0366cdf93b23c83a3d44
Sina Roydel <3inaroydl@gmail.com>
2024-02-15 13:56:16 +0330

    add gallery image model to admin panel

--- backend/admin.py
+++ backend/admin.py
@@ -6,6 +6,8 @@ admin.site.register(models.Tag)
 admin.site.register(models.TagPost)
 admin.site.register(models.RestaurantItem)
 admin.site.register(models.TeamMember)
+admin.site.register(models.GalleryImage)
+
 
 
 
