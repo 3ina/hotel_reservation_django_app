4973eb3dc8f2dfe592c24fcb91ffff90ac002a46
Sina Roydel <3inaroydl@gmail.com>
2024-02-17 11:59:37 +0330

    add comment model to admin panel

--- backend/admin.py
+++ backend/admin.py
@@ -7,6 +7,8 @@ admin.site.register(models.TagPost)
 admin.site.register(models.RestaurantItem)
 admin.site.register(models.TeamMember)
 admin.site.register(models.GalleryImage)
+admin.site.register(models.Comment)
+
 
 
 
