58f06fc5b014fe7f530c1a4a0a8a9f0658da23e9
Sina Roydel <3inaroydl@gmail.com>
2024-02-17 12:06:52 +0330

    add create_comment view

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
 from django.shortcuts import render
 from backend import models
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 
 def index(request):
@@ -77,3 +81,32 @@ class ListBlogPost(ListView):
     model = models.Post
     paginate_by = 3
     template_name = "backend/blog.html"
+
+
+def detail_blog_post(request, pk):
+    post = get_object_or_404(models.Post, id=pk)
+    tags = models.TagPost.objects.filter(post=post)
+    comments = models.Comment.objects.filter(post=post)
+
+    context = {
+        "post": post,
+        "tags": tags,
+        "comments": comments,
+    }
+    return render(request, "backend/blog-detail.html", context)
+
+
+@login_required
+@require_POST
+def create_comment(request,pk):
+    post = get_object_or_404(models.Post, id=pk)
+    user = request.user
+    comment_is_exists = models.Comment.objects.filter(post=post,author=user)
+    if comment_is_exists.exists():
+        messages.error(request, "you can write only one comment")
+        return detail_blog_post(request,pk)
+    text = request.POST['message']
+    comment = models.Comment.objects.create(post=post,author=user,text=text)
+    comment.save()
+    messages.success(request, "success")
+    return detail_blog_post(request, pk)
